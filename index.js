4558835c01683e54508eb766deee07ff9220a9ca
ana-vela <ana@anavela.net>
2021-06-23 14:39:35 -0600

    Adding dotenv file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 4000;
+require('dotenv').config();
+const port = process.env.PORT; 
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
