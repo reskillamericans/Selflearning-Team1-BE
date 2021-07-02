67d081607726f54fa337405ccc7114fea30601dc
Richard Dixon <43769645+dontbecypher16@users.noreply.github.com>
2021-07-01 22:34:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,7 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT; 
 
-const mongoose = require('mongoose')
-const connectionString = process.env.DB_URL
-
-    mongoose.connect(connectionString,
-  {useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false}, (err) => {
-     if(err)  {
-         console.log(err)
-     } else {
-         console.log('database connection successful')
- 
-     }
-   })
-
+const dbSetup = require('./database/setup')
 
 
 
