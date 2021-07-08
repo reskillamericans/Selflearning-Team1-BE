a98c928b01cc08b6d3fbbc7b9904b020ecd87b59
ana-vela <ana@anavela.net>
2021-07-08 10:10:53 -0600

    verifying password is added when signing up

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,57 +1,57 @@
-const User = require("../models/user");
-const bcrypt = require("bcrypt");
-const { createToken } = require("../services/jwtService");
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const { createToken } = require('../services/jwtService');
 
 exports.registerNewUser = (req, res) => {
-  if (!req.body.firstName) {
-    return res.status(400).json({ message: "Please enter your first name" });
-  } else if (!req.body.lastName) {
-    return res.status(400).json({ message: "Please enter your last name" });
-  } else if (!req.body.email) {
-    return res.status(400).json({ message: "Please enter your email address" });
-  };
+	if (!req.body.firstName) {
+		return res.status(400).json({ message: 'Please enter your first name' });
+	} else if (!req.body.lastName) {
+		return res.status(400).json({ message: 'Please enter your last name' });
+	} else if (!req.body.email) {
+		return res.status(400).json({ message: 'Please enter your email address' });
+	} else if (!req.body.password) {
+		return res.status(400).json({ message: 'Please enter your password' });
+	}
 
-  User.create(
-    {
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-    },
-    (err, newUser) => {
-      if (err) {
-        return res.status(500).json({ err });
-      } else {
-        bcrypt.genSalt(10, (err, salt) => {
-          if (err) {
-            return res.status(500).json({ err });
-          }
-          bcrypt.hash(req.body.password, salt, (err, hashedPassword) => {
-            if (err) {
-              return res.status(500).json({ err });
-            }
-            newUser.password = hashedPassword;
-            newUser.save((err, savedUser) => {
-              if (err) {
-                return res.status(500).json({ err });
-              }
-              let token = createToken(newUser);
-              if (!token) {
-                return res
-                  .status(500)
-                  .json({
-                    message:
-                      "Apologies, we cannot authenticate you. Please login",
-                  });
-              }
+	User.create(
+		{
+			firstName: req.body.firstName,
+			lastName: req.body.lastName,
+			email: req.body.email,
+			password: req.body.password,
+		},
+		(err, newUser) => {
+			if (err) {
+				return res.status(500).json({ err: 'general error' });
+			} else {
+				bcrypt.genSalt(10, (err, salt) => {
+					if (err) {
+						return res.status(500).json({ err });
+					}
+					bcrypt.hash(req.body.password, salt, (err, hashedPassword) => {
+						if (err) {
+							return res.status(500).json({ err });
+						}
+						newUser.password = hashedPassword;
+						newUser.save((err, savedUser) => {
+							if (err) {
+								return res.status(500).json({ err });
+							}
+							let token = createToken(newUser);
+							if (!token) {
+								return res.status(500).json({
+									message: 'Apologies, we cannot authenticate you. Please login',
+								});
+							}
 
-              return res.status(200).json({
-                message: "Success! You are now registered.",
-                token,
-              });
-            });
-          });
-        });
-      }
-    }
-  );
+							return res.status(200).json({
+								message: 'Success! You are now registered.',
+								token,
+							});
+						});
+					});
+				});
+			}
+		}
+	);
 };
