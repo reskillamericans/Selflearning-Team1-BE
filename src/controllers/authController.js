e247e20607ef7ad4db74bbebd68ae4dcca793b3d
ana-vela <ana@anavela.net>
2021-07-07 20:12:27 -0600

    fixing typos and updated index.js path in package.json

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,49 +1,57 @@
-const User = require("..models/user");
+const User = require("../models/user");
 const bcrypt = require("bcrypt");
-const { createToken } = require ("../services/jwtService")
+const { createToken } = require("../services/jwtService");
 
 exports.registerNewUser = (req, res) => {
+  if (!req.body.firstName) {
+    return res.status(400).json({ message: "Please enter your first name" });
+  } else if (!req.body.lastName) {
+    return res.status(400).json({ message: "Please enter your last name" });
+  } else if (!req.body.email) {
+    return res.status(400).json({ message: "Please enter your email address" });
+  };
 
-    if (!req.body.firstName) {
-        return res.status(400).json({message: 'Please enter your first name'})
-    } else if (!req.body.lastName) {
-        return res.status(400).json({message: 'Please enter your last name'})
-    } else if (!req.body.email) {
-        return res.status(400).json({message: 'Please enter your email address'})
-    }
-
-    User.create({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-     }),
+  User.create(
+    {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+    },
     (err, newUser) => {
-        if (err) {
-            return res.status(500).json({err});
-        }  else {
+      if (err) {
+        return res.status(500).json({ err });
+      } else {
         bcrypt.genSalt(10, (err, salt) => {
-            if(err) {
-                return res.status(500).json({err});
+          if (err) {
+            return res.status(500).json({ err });
+          }
+          bcrypt.hash(req.body.password, salt, (err, hashedPassword) => {
+            if (err) {
+              return res.status(500).json({ err });
             }
-            bcrypt.hash(req.body.password, salt, (err, hashedPassword) => {
-                if (err) {
-                    return res.status(500).json({err});
-                }
-                newUser.password = hashedPassword;
-                newUser.save((err, savedUser) => {
-                    if (err) {
-                        return res.status(500).json({err});
-                    }
-                    let token = createToken(newUser);
-                    if (!token) {
-                        return res.status(500).json({message: "Apologies, we cannot authenticate you. Please login",});
-                    }
+            newUser.password = hashedPassword;
+            newUser.save((err, savedUser) => {
+              if (err) {
+                return res.status(500).json({ err });
+              }
+              let token = createToken(newUser);
+              if (!token) {
+                return res
+                  .status(500)
+                  .json({
+                    message:
+                      "Apologies, we cannot authenticate you. Please login",
+                  });
+              }
 
-                    return res.status(200).json({
-                        message: "Success! You are now registered.", token,
-                    });
-                })
-            })
-     } )}
+              return res.status(200).json({
+                message: "Success! You are now registered.",
+                token,
+              });
+            });
+          });
+        });
+      }
     }
-}
+  );
+};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const express = require("express");
 const router = express.Router();
 
-const authController = require("../controllers/authConroller");
+const authController = require("../controllers/authController");
 router.post("/signup", authController.registerNewUser);
 
 module.exports = router;
