adebdc7791436f0c7d29748baf4f0b61123b0f19
meleciosilva <mpsilva94@gmail.com>
2021-07-08 22:10:26 -0400

    add step router

---
+++ src/routers/steps.router.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const controller = require("../controllers/steps.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+
+router
+  .route("/courseId/stepId")
+  .put(controller.addStep)
+  .delete(controller.removeStep)
+  .all(controller.methodNotAllowed);
+
+router
+  .route("/stepId")
+  .get(controller.read)
+  .put(controller.update)
+  .delete(controller.destroy)
+  .all(methodNotAllowed);
+
+router
+  .route("/")
+  .post(controller.create)
+  .get(controller.list)
+  .all(methodNotAllowed);
+
+module.exports = Router;
