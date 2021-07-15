3d80d1ec35816ee03e9d36a10c867771736789c9
meleciosilva <mpsilva94@gmail.com>
2021-07-15 08:37:56 -0400

    change 'courseId' to 'course'

--- src/models/steps.model.js
+++ src/models/steps.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const stepSchema = new Schema({
-  courseId: {
+  course: {
     type: Schema.Types.ObjectId,
     ref: 'Course' 
   },
