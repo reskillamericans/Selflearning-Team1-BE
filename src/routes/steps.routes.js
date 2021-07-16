93d20541ae502360917b21442ba873da2347bd9e
meleciosilva <mpsilva94@gmail.com>
2021-07-16 18:02:17 -0400

    add authenticateUser middleware to routes

--- src/routes/steps.routes.js
+++ src/routes/steps.routes.js
@@ -1,24 +1,25 @@
 const router = require("express").Router();
 const controller = require("../controllers/steps.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const { authenticateUser } = require("../middlewares/authentication");
 
 router
   .route("/:courseId/:stepId")
-  .put(controller.addStep)
-  .delete(controller.removeStep)
+  .put(authenticateUser, controller.addStep)
+  .delete(authenticateUser, controller.removeStep)
   .all(methodNotAllowed);
 
 router
   .route("/:stepId")
-  .get(controller.read)
-  .put(controller.update)
-  .delete(controller.destroy)
+  .get(authenticateUser, controller.read)
+  .put(authenticateUser, controller.update)
+  .delete(authenticateUser, controller.destroy)
   .all(methodNotAllowed);
 
 router
   .route("/")
-  .post(controller.create)
-  .get(controller.list)
+  .post(authenticateUser, controller.create)
+  .get(authenticateUser, controller.list)
   .all(methodNotAllowed);
 
 module.exports = router;
