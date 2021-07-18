e75e2f0cfe8995632147bc00e7d2b1f7d8247a9a
meleciosilva <mpsilva94@gmail.com>
2021-07-18 10:41:59 -0400

    move addStep and removeStep to course controller

--- src/controllers/steps.controller.js
+++ src/controllers/steps.controller.js
@@ -1,65 +1,10 @@
 const Step = require("../models/steps.model");
-const Course = require("../models/courses.model")
-
-// Validation Middleware
-
-// Checks if step exists
-function stepExists(req, res, next) {
-  const { stepId } = req.params;
-  Step.findById(stepId, (err, step) => {
-    if (err) return next();
-    if (!step) return next({
-      status: 404,
-      message: `Step id ${req.params.stepId} cannot be found`
-    });
-    res.locals.step = step;
-    next();
-  });
-}
 
 // Create Step
 function create(req, res, next) {
   Step.create({ ...req.body }, (err, newStep) => {
     if (err) return next(err);
     res.status(201).json({ message: "New step created", newStep });
-  });
-}
-
-// Add Step to course
-function addStep(req, res, next) {
-  const { step } = res.locals;
-  Course.findByIdAndUpdate(req.params.courseId, { $addToSet: { steps: step } }, {new: true}, (err, course) => {
-    if (err) return next();
-    if (!course) return next({
-      status: 404,
-      message: `Course id ${req.params.courseId} cannot be found`
-    });
-    course.save((err, savedCourse) => {
-      if (err) return next({
-        status: 400,
-        message: err
-      });
-      res.json({ message: "Step added to course", savedCourse });
-    });
-  });
-}
-
-// Remove step from course
-function removeStep(req, res, next) {
-  const { step } = res.locals;
-  Course.findByIdAndUpdate(req.params.courseId, { $pull: { steps: step._id } }, {new: true}, (err, course) => {
-    if (err) return next();
-    if (!course) return next({
-      status: 404,
-      message: `Course id ${req.params.courseId} cannot be found`
-    });
-    course.save((err, savedCourse) => {
-      if (err) return next({
-        status: 400,
-        message: err
-      });
-      res.json({ message: "Step removed from course", savedCourse });
-    })
   });
 }
 
@@ -118,8 +63,6 @@ function destroy(req, res, next) {
 
 module.exports = {
   create,
-  addStep: [stepExists, addStep],
-  removeStep: [stepExists, removeStep],
   list,
   read,
   update,
