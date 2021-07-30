9ba1e3154eb36ae94d263825d441e9bf48898cb8
Dorian Edwards <de.pharmd2018@gmail.com>
2021-07-30 08:22:59 -0500

    add request model

---
+++ src/models/request.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const requestSchema = new Schema(
+  {
+    step: { type: Schema.Types.ObjectId, ref: 'Step' },
+    student: { type: Schema.Types.ObjectId, ref: 'User' },
+    status: { type: String, enum: ['pending', 'accepted', 'closed'], default: 'pending' },
+    mentor: { type: Schema.Types.ObjectId, ref: 'User' },
+  },
+  { timestamps: true }
+)
+module.exports = mongoose.model('Request', requestSchema)
