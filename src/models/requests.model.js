9338ecba34cb6a8bcbeabe9cec1561dc21482581
Dorian Edwards <de.pharmd2018@gmail.com>
2021-08-02 21:26:57 -0400

    add request model

---
+++ src/models/requests.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const requestSchema = new Schema(
+  {
+    step: { type: Schema.Types.ObjectId, ref: "Step" },
+    course: { type: Schema.Types.ObjectId, ref: "Course" },
+    student: { type: Schema.Types.ObjectId, ref: "User" },
+    status: { type: String, enum: ["pending", "accepted", "closed"], default: "pending" },
+    mentor: { type: Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("Request", requestSchema);
