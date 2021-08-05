984ed2708202dd3a509ad87ac982d23aee349c7c
Dorian Edwards <de.pharmd2018@gmail.com>
2021-08-05 14:14:36 -0400

    install express-validator, add minLength to password & change email to lowercase

--- src/models/user.js
+++ src/models/user.js
@@ -1,29 +1,31 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const userSchema = new Schema( {
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    }, 
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        required: true,
-        enum: ['student', 'mentor'], 
-    },
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: [6, "Password must be at least 6 characters long"],
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["student", "mentor"],
+  },
 });
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
