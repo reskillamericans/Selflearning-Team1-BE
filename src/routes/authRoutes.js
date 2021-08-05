6e29367ed0f1e59d27be774a85091725cbf339c0
Dorian Edwards <de.pharmd2018@gmail.com>
2021-08-05 16:53:24 -0400

    add validateUser middleware, & use express-validator on user model

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,22 @@
 const { Router } = require("express");
 const express = require("express");
 const router = express.Router();
+const validateUser = require("../middlewares/validateUser");
+const { body } = require("express-validator");
 
 const authController = require("../controllers/authController");
-router.post("/signup", authController.registerNewUser);
+router.post(
+  "/signup",
+  body("firstName").not().isEmpty().withMessage("Please enter your first name"),
+  body("lastName").not().isEmpty().withMessage("Please enter your last name"),
+  body("email").isEmail().withMessage("Please enter a valid email address"),
+  body("password")
+    .not()
+    .isEmpty()
+    .withMessage("Please enter a password at least 6 characters long"),
+  body("role").not().isEmpty().withMessage("Please select a role"),
+  validateUser,
+  authController.registerNewUser
+);
 
 module.exports = router;
